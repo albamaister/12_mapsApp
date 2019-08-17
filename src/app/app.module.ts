6f64c28954a212922c4c39ca20271fc38a062732
Andres Alba <bryanalba92@gmail.com>
2019-08-17 12:40:44 -0500

    Creación de componente de mapas y mostrar mapa de Google

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,16 +4,23 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './material.module';
+import { MapaComponent } from './components/mapa/mapa.component';
+
+import { AgmCoreModule } from '@agm/core';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MapaComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    MaterialModule
+    MaterialModule,
+    AgmCoreModule.forRoot({
+      apiKey: 'AIzaSyDZOCYeovjX8O6SEYDQWcZLv2LcDuDdq4M'
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
